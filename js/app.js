01e8b2c877342efb3ea4f24b9379ea38ba61feb7
Kirco <stoimenov.kircho@gmail.com>
2020-05-21 13:34:29 +0200

    st

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $().ready(function () {
   function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType('application/json');
-    rawFile.open('GET', file, true);
+    rawFile.open('GET', 'js/pictures.json', true);
     rawFile.onreadystatechange = function () {
       if (rawFile.readyState === 4 && rawFile.status == '200') {
         callback(rawFile.responseText);
@@ -14,12 +14,16 @@ $().ready(function () {
   readTextFile('js/pictures.json', function (text) {
     const data = JSON.parse(text);
     const list = data.photos;
-    console.log(list[1].captions)
 
-
-
-
-
-  })
-
+    $('.img-container').on('click', 'img', function () {
+      $(list).each(function () {
+        const $picName = this.name;
+        console.log(this.url);
+        $(this).each(function () {
+          console.log($picName);
+        });
+      });
+    });
+  });
+  //
 });
