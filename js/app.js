4e1a8796ca106d136fd95f9aa5489060c38b2260
Kirco <stoimenov.kircho@gmail.com>
2020-05-22 10:55:46 +0200

    tests

--- js/app.js
+++ js/app.js
@@ -11,6 +11,13 @@ $().ready(function () {
     rawFile.send(null);
   }
 
+  $('.display').css('display', 'none')
+  const hideD = function () {
+    $('.display').click(function () {
+      $('.display').hide();
+    })
+  };
+
   readTextFile('js/pictures.json', function (text) {
     const data = JSON.parse(text);
     const list = data.photos;
@@ -19,25 +26,42 @@ $().ready(function () {
       for (let i = 0; i < list.length; i++) {
         const altText = list[i].alttext;
         const caption = list[i].captions;
-        const img = list[i].url;
-        const displayContent = $(`<div class="popUp"><span class='x'>x</span><span class='goLeft'><</span><span class="goRight">></span><img class='currentImg' src="${img}"><span class='img-caption'>${caption}</span></div>`);
+        const $img = list[i].url;
+        const dispayContent = $('.display');
 
         if ($(this).attr('alt') === altText) {
-          console.log(caption);
-          $('#gallery-container').prepend(displayContent);
+
+          dispayContent.html(`<img src="${$img}" alt="${altText}" class='active'> <span class='captions'>${caption}</span><span class="x">X</span><span class="goLeft"><</span><span class="goRight">></span>`).show()
+
 
           $('.goRight').on('click', function () {
-            $("#gallery-container").next($('.img-container')).append(displayContent)
-            console.log($('img').attr('alt'))
+            let currentIndex = list.indexOf(list[i]);
+            let nextIndex = (currentIndex + 1) % list.length;
+
+            $('.active').attr('src', list[nextIndex].url)
+            if (list[nextIndex].alttext) {
+
+              $('.active').attr('src', list[nextIndex].url)
+              console.log(list[nextIndex].alttext)
+
+              //test
+              list.forEach((item, index, arr) => {
+                console.log("Current: " + item.alttext);
+                console.log("Previous: " + ((0 === index) ? "START" : arr[index - 1].name));
+                console.log("Next: " + ((arr.length - 1 === index) ? "END" : arr[index + 1].name));
+              });
+            }
+          })
+
+          $('.x').click(function () {
+            $('.display').hide()
           })
         }
 
 
 
 
-        $('.x').click(function () {
-          $(displayContent).hide()
-        })
+
 
 
 
